26febd81f70fe7b7727f5a68727c375fd0820217
Mark Leidman <marksleidmans@flynowpaylater.com>
2023-08-19 17:39:56 +0300

    old form values

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,6 +19,7 @@
                     <div class="mt-2">
                         <input id="email" name="email" type="email" autocomplete="email" required
                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                               value="{{ old('email') }}"
                                data-validate-email>
                     </div>
                 </div>
